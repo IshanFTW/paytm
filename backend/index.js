c506f6dd9250f1bfbfdde3c1747e6de2323aab2b
IshanFTW <ishanjindal2003@gmail.com>
2024-02-05 22:48:58 +0530

    Added cors, body parser and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+const mainRouter = require("./routes/index")
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/vi", mainRouter);
+
+app.listen(port, () => {
+    console.log("Server is running")
+})
 
 
