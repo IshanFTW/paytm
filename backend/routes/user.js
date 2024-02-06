85a639279d788d05284b521f78891d4f47a9902e
IshanFTW <ishanjindal2003@gmail.com>
2024-02-06 22:37:18 +0530

    added signin and signup route to user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const {User} = require('../db')
+const {User} = require('../db');
+const zod = require('zod');
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = require('./config')
 
+const signupSchema = zod.object({
+    firstName: zod.string(),
+    lastName: zod.string(),
+    userName: zod.string(),
+    password: zod.string()
+})
 
 router.post('/signup', async (req, res) => {
     try{
         const {firstName, lastName, userName, password} = req.body;
+        const {success} = signupSchema.safeParse(req.body);
+        if(!success){
+            return res.status(400).json({
+                message: "Incorrect inputs"
+            })
+        }
         const existingUser = await User.findOne({userName: userName});
         if(existingUser){
             return res.status(400).json({message: "User already exists"})
@@ -17,13 +32,36 @@ router.post('/signup', async (req, res) => {
                 password: password
 
             })
-            res.status(200).json({message: "Signed Up"})
         }
-        
+        const userId = User._id;
+
+        const token = jwt.sign({userId}, JWT_SECRET);
+        res.json({
+            message: "User Created Successful",
+            token: token
+        })
     }
     catch(e){
         res.status(500).send(e.message)
     }
+})
+
+router.post('/signin', async (req, res) => {
+    try{
+        const {userName, password} = req.body;
+        const existingUser = await User.findOne({userName: userName, password: password});
+        if (existingUser){
+            const userId = User._id;
+            const token = jwt.sign({userId}, JWT_SECRET);
+            res.status(200).json({
+                token: token
+            })
+        }
+    }catch(e){
+        res.status(500).send(e.message)
+    }
+
+
 })
 
 module.exports = router;
