dd0c2fcc00a87c5666fe5095c8d81a82b6ca1715
IshanFTW <ishanjindal2003@gmail.com>
2024-02-13 12:02:12 +0530

    create account schema

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const {User} = require('../db');
 const zod = require('zod');
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = require('./config')
+const authMiddleware = require('./middleware');
 
 const signupSchema = zod.object({
     firstName: zod.string(),
@@ -53,15 +54,60 @@ router.post('/signin', async (req, res) => {
         if (existingUser){
             const userId = User._id;
             const token = jwt.sign({userId}, JWT_SECRET);
-            res.status(200).json({
+            res.status(200).json({  
                 token: token
             })
         }
     }catch(e){
         res.status(500).send(e.message)
     }
+})
 
+const updateBody = zod.object({
+	password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put('/' ,authMiddleware ,async (req, res) => {
+    try{
+        const {success} = updateBody.safeParse(req.body);
+        if(!success){
+            res.status(411).json({message: "Error while updating information"})
+        }
+        await User.updateOne(req.body, {
+            _id: req.userId
+        })
+       
+    }catch(e){
+        return res.status(400).send(e.message);
+    }
+    res.status(200).json({message: "Updated successfully"})
 
 })
 
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
 module.exports = router;
